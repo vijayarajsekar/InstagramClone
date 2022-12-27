ea4a281f27ba71d1b3d12957f7e0ccf6afefd6fe
vsekar <vsekar@visteon.com>
2022-12-27 15:44:48 +0530

    # Added circle image # Completed layout design

--- app/src/main/java/com/insta/app/MainActivity.kt
+++ app/src/main/java/com/insta/app/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.insta.app
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.insta.app.Fragments.HomeFragment
+import com.insta.app.Fragments.NotificationsFragment
+import com.insta.app.Fragments.ProfileFragment
+import com.insta.app.Fragments.SearchFragment
 import com.insta.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Variable Declaration
+     */
     private lateinit var mBinding: ActivityMainBinding
-    private lateinit var mTitle: TextView
+    internal var mSelectedFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,33 +24,43 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        mTitle = mBinding.title
-
+        /**
+         * Initialize and Setting BottomNavigationView fragments
+         */
         val mNavView: BottomNavigationView = mBinding.navView
         mNavView.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.navigation_home -> {
-                    mTitle.setText("Home")
-                    return@setOnItemSelectedListener true
+                    mSelectedFragment = HomeFragment()
                 }
                 R.id.navigation_search -> {
-                    mTitle.setText("Search")
-                    return@setOnItemSelectedListener true
+                    mSelectedFragment = SearchFragment()
                 }
                 R.id.navigation_add_post -> {
-                    mTitle.setText("Add Post")
-                    return@setOnItemSelectedListener true
+                    mSelectedFragment = NotificationsFragment()
                 }
                 R.id.navigation_notification -> {
-                    mTitle.setText("Favourite")
-                    return@setOnItemSelectedListener true
+                    mSelectedFragment = NotificationsFragment()
                 }
                 R.id.navigation_profile -> {
-                    mTitle.setText("Profile")
-                    return@setOnItemSelectedListener true
+                    mSelectedFragment = ProfileFragment()
                 }
             }
+
+            /**
+             * Setting selected fragment as per the click
+             */
+            if (mSelectedFragment != null)
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container_view, mSelectedFragment!!).commit()
+
             false
         }
+
+        /**
+         * Setting Default page as HomeScreenFragment
+         */
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, HomeFragment()).commit()
     }
 }
